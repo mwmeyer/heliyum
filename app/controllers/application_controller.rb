551c760534ebe45813480f1eeab3dbe1030c2a6f
Matt Meyer <matthewwilliammeyer@gmail.com>
2025-09-11 09:34:07 -0400

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  helper_method :admin?
-
-  private
-
-  def admin?
-    authenticate_with_http_basic do |username, password|
-      username == (ENV['ADMIN_USERNAME'] || 'admin') &&
-      password == (ENV['ADMIN_PASSWORD'] || 'password')
-    end
-  end
 end
